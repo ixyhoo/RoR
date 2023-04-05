0c343be4ab8c1e34427e7359768550c6790e4660
ixyhoo <ixyhoo@outlook.fr>
2023-04-05 21:09:10 +0200

    make the model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get "/salut", to: "pages#salut"
+
 root to: "pages#home"
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/salut(/:name)", to: "pages#salut", as: "salut"
 end
