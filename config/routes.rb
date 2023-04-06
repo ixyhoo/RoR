3baac2d2d4ec177a495cc3c48168aafe2e8ab9ad
ixyhoo <ixyhoo@outlook.fr>
2023-04-06 10:40:06 +0200

    error post#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 root to: "pages#home"
   # Defines the root path route ("/")
   # root "articles#index"
   get "/salut(/:name)", to: "pages#salut", as: "salut"
+  get "/articles", to: "posts#index", as: "posts"
 end
