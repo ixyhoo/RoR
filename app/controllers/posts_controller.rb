795ac853c03ad2a74a4edbdb90b2289755885192
ixyhoo <ixyhoo@outlook.fr>
2023-04-08 14:34:26 +0200

    error js

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,5 +23,16 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.create(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
   
 end
